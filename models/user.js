e1ee244bd9d9c1521e342410d3e474cc905ade31
IthielKyama <nyagaithiel@gmai.com>
2025-06-26 17:02:07 +0300

    add cart functionality

--- models/user.js
+++ models/user.js
@@ -1,16 +1,99 @@
-const Sequelize = require('sequelize');
+const mongodb = require("mongodb");
+const getDb = require("../util/database").getDb;
 
-const sequelize = require('../util/database');
+const ObjectId = mongodb.ObjectId;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+class User {
+  constructor(username, email, cart, id) {
+    this.name = username;
+    this.email = email;
+    this.cart = cart;
+    this._id = id;
+  }
+
+  save() {
+    const db = getDb();
+    return db
+      .collection("users")
+      .insertOne(this)
+      .then((result) => {
+        console.log("User Created");
+      })
+      .catch((err) => {
+        console.error("Error creating user:", err);
+      });
+  }
+
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items];
+    if (cartProductIndex >= 0) {
+      // Product already exists in the cart
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      // Product does not exist in the cart
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
+    const updatedCart = { items: updatedCartItems };
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      )
+      .then((result) => {
+        console.log("Product added to cart");
+      })
+      .catch((err) => {
+        console.error("Error adding product to cart:", err);
+      });
+  }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map((i) => {
+      return i.productId;
+    });
+    return db
+      .collection("products")
+      .find({ _id: { $in: productIds } })
+      .toArray()
+      .then((products) => {
+        return products.map((p) => {
+          return {
+            ...p,
+            quantity: this.cart.items.find((i) => {
+              return i.productId.toString() === p._id.toString();
+            }).quantity
+          };
+        });
+      })
+      .catch((err) => {
+        console.error("Error fetching cart:", err);
+      });
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .findOne({ _id: new ObjectId(userId) })
+      .then((user) => {
+        console.log(user);
+        return user;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+}
 
 module.exports = User;
